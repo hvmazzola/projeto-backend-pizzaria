d65f7662ebd93a301078afea7bb96b319077669d
hvmazzola <98432153+hvmazzola@users.noreply.github.com>
2023-04-10 17:57:44 -0300

    feat: adicionar pacote bcrypt + criptografia na senha do user

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,4 +1,5 @@
 const mongoose = require ("mongoose");
+const bcrypt = require ("bcrypt");
 
 const UsuarioSchema = new mongoose.Schema({
     nome: { type: String, required: true },
@@ -25,6 +26,13 @@ const UsuarioSchema = new mongoose.Schema({
         }
     ],
     admin: { type: Boolean, required: true, default: false }
+});
+
+UsuarioSchema.pre("save", async function(next) {
+    if(this.senha){
+        this.senha = await bcrypt.hash(this.senha, 10);
+    }
+    next();
 });
 
 const Usuario = mongoose.model("usuarios", UsuarioSchema);
