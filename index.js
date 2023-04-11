9a44b356699f49d95856ac5d3c9cfdbe6077e344
hvmazzola <98432153+hvmazzola@users.noreply.github.com>
2023-04-11 16:25:45 -0300

    feat: autenticar usuário com jwt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectToDatabase = require("./src/database/database"); // arquivo de conexão com o banco
 const usuario = require("./src/router/usuario.router"); // arquivo de rotas do usuário
+const auth = require("./src/router/auth.router"); // arquivo de rotas de autenticação
+
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.use(express.json());
 connectToDatabase(); // conectanco com o banco
 
 app.use("/usuario", usuario); // chamando rotas do usuário
+app.use("/auth", auth); // chamando rotas de autenticação
 
 app.get ("/", (req, res) => {
     res.send({
---
+++ src/controller/auth.controller.js
@@ -0,0 +1,27 @@
+const authService = require("../service/auth.service");
+const bcrypt = require("bcrypt");
+
+const loginController = async (req, res) => {
+    const {email, senha} = req.body;
+
+    const user = await authService.loginService(email);
+
+    if(!user){
+        return res.status(400).send({ message: "Usuário não encontrado. Tende novamente"});
+    }
+
+    const testeSenha = await bcrypt.compare(senha, user.senha);
+
+    if(!testeSenha){
+        return res.status(400).send({ message: "Senha inválida. Tende novamente"});
+    }
+
+    const token = authService.generateToken(user.id);
+
+    return res.status(200).send({
+        email,
+        token
+    });
+}
+
+module.exports = { loginController };
