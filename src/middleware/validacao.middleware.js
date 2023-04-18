89e9630410d6a32d98105c2f83902395483015b0
hvmazzola <98432153+hvmazzola@users.noreply.github.com>
2023-04-18 11:26:33 -0300

    feat: validar Login

--- src/middleware/validacao.middleware.js
+++ src/middleware/validacao.middleware.js
@@ -126,10 +126,28 @@ const validaId = (req, res, next) => {
         return next();
     }else{
         return res.status(400).send({ message: `O ID que foi passado não corresponde aos podrões necessários. Tente novamente!`});
+    }    
+};
+
+const validaLogin = (req, res, next) => {
+
+    let erros = [];
+
+    if(req.body.email == null){
+        erros.push("email");
     }
-    
-    
-    
+
+    if(req.body.senha == null){
+        erros.push("senha");
+    }
+
+    if(erros.length == 0) {
+        return next();
+    } else if(erros.length == 1) {
+        return res.status(400).send({ message: `O campo ${erros} precisa ser preenchido. Tente novamente!`});
+    } else {
+        return res.status(400).send({ message: `Os campos ${erros} precisam ser preenchidos. Tente novamente!`});
+    }; 
 };
 
 module.exports = {
@@ -139,5 +157,6 @@ module.exports = {
     validaSabor,
     validaPedido,
     validaCarrinho,
-    validaId
+    validaId,
+    validaLogin
 };
